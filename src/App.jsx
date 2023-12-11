93ffd6c4f5d75c09a9c5be8195de6b42bc59442c
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-12-11 08:39:02 +0530

    Apply Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import MainLayout from "./components/MainLayout";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-      <h1>Reservation Application</h1>
-  ); 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/admin" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
