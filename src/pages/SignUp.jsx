4a9207bddf47b965037787e6d082a1ee7401d19b
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-12-11 08:52:31 +0530

    Apply Sign Up Page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import { AddCircle } from "@mui/icons-material";
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  Paper,
+  Radio,
+  RadioGroup,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React from "react";
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const paperStyle = { padding: "30px 20px", width: 300, margin: "20px auto" };
+  const headerStyle = { margin: 0 };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const marginTop = { marginTop: 5 };
 
-export default SignUp
+  return (
+    <Grid>
+      <Paper elevation={20} style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <AddCircle />
+          </Avatar>
+          <h2 style={headerStyle}>Sign Up</h2>
+          <Typography variant="caption" gutterBottom>
+            Please fill this form to create an account
+          </Typography>
+        </Grid>
+        <form>
+          <TextField fullWidth label="Name" placeholder="Enter your name" />
+          <TextField fullWidth label="Email" />
+          <TextField fullWidth label="Mobile" />
+          <FormControl component="fieldset" style={marginTop}>
+            <FormLabel component="legend">Gender</FormLabel>
+            <RadioGroup
+              aria-label="gender"
+              name="gender"
+              style={{ display: "initial" }}
+            >
+              <FormControlLabel
+                value="female"
+                control={<Radio />}
+                label="Female"
+              />
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+            </RadioGroup>
+          </FormControl>
+          <TextField fullWidth label="Password" />
+          <TextField fullWidth label="Confirm Password" />
+          <FormControlLabel
+            control={<Checkbox name="checkbox" />}
+            label="I accept the terms and conditions"
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Sign Up
+          </Button>
+        </form>
+      </Paper>
+    </Grid>
+  );
+};
+
+export default SignUp;
